2f893c0190552a2769b7dd6bd5be662431b6cf01
Ireh-Jo <kingslanding@naver.com>
2022-02-23 00:45:39 +0900

    Bean Validation 추가 뷰 컨트롤러 선언

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -2,11 +2,18 @@ package tacos;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class Taco {
 
+	@NotNull
+	@Size(min=5, message="이름은 최소 5글자 이상 입력해주시기 바랍니다.")
 	private String name;
+	
+	@Size(min=1, message="재료는 최소 1개 이상 선택해주시기 바랍니다.")
 	private List<String> ingredients;
 }
