75cc3aeaa3df8217133de6ca85ff4d104cce735e
Ireh-Jo <kingslanding@naver.com>
2022-03-04 22:54:58 +0900

    스프링 시큐리티에 내장된 사용자 스토어 공부(인메모리, JDBC, LDAP) 완료

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,21 +1,16 @@
 package tacos.security;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	DataSource dataSource;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -28,19 +23,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		/*
-		 * auth.inMemoryAuthentication() .withUser("user1") .password("{noop}password1")
-		 * .authorities("ROLE_USER") .and() .withUser("user2")
-		 * .password("{noop}password2") .authorities("ROLE_USER");
-		 */
-		auth.jdbcAuthentication()
-			.dataSource(dataSource)
-			.usersByUsernameQuery(
-					"select username, password, enabled from users " +
-					"where username=?")
-			.authoritiesByUsernameQuery(
-					"select username, autority from authorities " +
-					"where username=?")
-			.passwordEncoder(new NoEncodingPasswordEncoder());
+		
+		// 아래 코드는 책의 LDAP 기반 사용자 스토어 예제 코드입니다.
+		auth
+		.ldapAuthentication()
+		.userSearchBase("ou=people")
+		.userSearchFilter("(uid={0})")
+		.groupSearchBase("ou=groups")
+		.groupSearchFilter("member={0}")
+		.contextSource()
+		.root("dc=tacocloud,dc=com")
+		.ldif("classpath:users.ldif")
+		.and()
+		.passwordCompare()
+		.passwordEncoder(new BCryptPasswordEncoder())
+		.passwordAttribute("userPasscode");
+		
+		// 아래 코드는 책의 JDBC 기반 사용자 스토어 예제 코드입니다.
+		/*@Autowired
+	    DataSource dataSource;
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery(
+			"select username, password, enabled from users " +
+			"where username=?")
+		.authoritiesByUsernameQuery(
+			"select username, authority from authorities " +
+			"where username=?")
+		.passwordEncoder(new NoEncodingPasswordEncoder());*/
+		
+		// 아래 코드는 책의 인메모리 기반 사용자 스토어 예제 코드입니다.
+		/*auth.inMemoryAuthentication()
+		.withUser("user1")
+		.password("{noop}password1")
+		.authorities("ROLE_USER")
+		.and()
+		.withUser("user2")
+		.password("{noop}password2")
+		.authorities("ROLE_USER");*/
 	}
 }
